1f93017ebe2928dec92404af87be0260152de8be
Teekky <xouang.sourignavong@gmail.com>
2021-11-18 16:45:42 +0100

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>HelloWorld</h1>
+  <ul>
+    <li v-for="item in items" :key="item.message">
+      {{ item.message }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      items: [{ message: "One Day" }, { message: "Two Days" }, { message: "Three Days" }, { message: "Four Days" }, { message: "Five Days" }, ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
