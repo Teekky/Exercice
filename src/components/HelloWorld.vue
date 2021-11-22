885ad9901d0f5b6cf6f8c131debb54cf2eee4274
Teekky <xouang.sourignavong@gmail.com>
2021-11-22 16:24:48 +0100

    hide and show fonctionnel

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,14 @@
 <template>
   <h1>HelloWorld</h1>
-  <ul>
+  <ul id="hide" v-if="seen">
     <li v-for="item in items" :key="item.message">
       {{ item.message }}
     </li>
   </ul>
+
+  <div id="app" v-on:click="seen = !seen" class="control">
+    <a id="button">Hide WATA</a>
+  </div>
 </template>
 
 <script>
@@ -12,7 +16,15 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      items: [{ message: "One Day" }, { message: "Two Days" }, { message: "Three Days" }, { message: "Four Days" }, { message: "Five Days" }, ],
+      items: [
+        { message: "One Day" },
+        { message: "Two Days" },
+        { message: "Three Days" },
+        { message: "Four Days" },
+        { message: "Five Days" },
+      ],
+      isHidden: false,
+      seen: true,
     };
   },
 };
@@ -32,6 +44,13 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: white;
 }
+
+#button {
+  background-color: #42b983;
+  padding: 1em;
+  cursor: pointer;
+}
+
 </style>
